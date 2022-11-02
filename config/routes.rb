4388766f8f6a606e42e70213b8235ced4a5a1aeb
polapob <poom12341@hotmail.com>
2022-11-02 23:42:17 +0700

    homework

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :inventories
+  get 'inventory/show'
   resources :items
   get 'main/login'
-  post 'main/login' ,to: "main#login_post"
-  get 'main/inventories'
-  get 'main/user_items', to:"items#index"
+  post 'main/login', to: 'main#login_post'
+  get 'main/user_items', to: 'items#index'
+  get 'main/inventories', to: 'inventories#index'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_074926) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_162612) do
+  create_table "inventories", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.float "price"
+    t.datetime "timestamp", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_inventories_on_item_id"
+    t.index ["user_id"], name: "index_inventories_on_user_id"
+  end
+
   create_table "items", force: :cascade do |t|
     t.integer "user_id", null: false
     t.float "price"
@@ -29,5 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_074926) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "inventories", "items"
+  add_foreign_key "inventories", "users"
   add_foreign_key "items", "users"
 end
